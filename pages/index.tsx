dcc523ae20560f8bb8028bcfc4689082331014c4
aidan-starke <starke.aidan@gmail.com>
2022-08-08 15:24:40 +1200

    Format post date, styling tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { GetServerSideProps, NextPage } from "next";
-import JSONPretty from "react-json-pretty";
 import {
 	fetcher,
 	GetPostsDocument,
@@ -7,6 +6,7 @@ import {
 	useGetPostsQuery,
 } from "@/libs/api/generated";
 import { client } from "@/libs/client";
+import { formatDistance } from "date-fns";
 
 export const getServerSideProps: GetServerSideProps = async () => {
 	const fetchData = fetcher(client, GetPostsDocument);
@@ -24,15 +24,22 @@ const Home: NextPage<{ posts: GetPostsQuery["posts"] }> = ({ posts }) => {
 		refetchIntervalInBackground: true,
 	});
 
+	const getDistance = (timestamp: string) =>
+		formatDistance(new Date(timestamp), new Date(), {
+			addSuffix: true,
+		});
+
 	return (
 		<div className="h-screen w-screen p-8 m-auto justify-center">
 			<div className="justify-center w-2/3 m-auto">
-				<h2 className="text-xl font-mono font-semibold mb-6">Posts</h2>
+				<h2 className="text-xl font-mono tracking-wide font-semibold mb-6">
+					Posts
+				</h2>
 				<div className="space-y-6">
 					{(data?.posts ?? posts).map((post) => (
 						<div
 							key={post.id}
-							className="p-4 border-2 border-pink-200 rounded space-y-2"
+							className="p-4 border-2 border-pink-200 rounded space-y-2 shadow"
 						>
 							<p className="font-mono text-lg">
 								<span className="border-b border-pink-300 p-1">
@@ -46,8 +53,9 @@ const Home: NextPage<{ posts: GetPostsQuery["posts"] }> = ({ posts }) => {
 									By: <span className="font-mono">{post.user.username}</span>
 								</p>
 								<p>
-									At:&nbsp;
-									<span className="font-mono text-xs">{post.created_at}</span>
+									<span className="font-mono text-xs">
+										{getDistance(post.created_at)}
+									</span>
 								</p>
 							</div>
 						</div>
